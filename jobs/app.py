57393fbb4073a28307bd5b71f75c7c63779e490a
echan1912 <echan1912@gmail.com>
2021-04-09 18:53:13 +0800

    typo

--- jobs/app.py
+++ jobs/app.py
@@ -55,8 +55,7 @@ def reviews(employer_id):
         status = request.form['status']
         date = datetime.datetime.now().strftime("%m/%d/%Y")
         execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)'
-        (review, rating, title, date, status, employer_id)
-        commit=True)
+        (review, rating, title, date, status, employer_id, commit=True)
     return render_template('review.html', employer_id=employer_id)
 
 
