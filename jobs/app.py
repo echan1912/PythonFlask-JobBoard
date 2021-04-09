6220ffb41d00577be3b0a09c27e11f2e8e35f23c
echan1912 <echan1912@gmail.com>
2021-04-09 17:08:35 +0800

    done

--- jobs/app.py
+++ jobs/app.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 def open_connection():
     connection = getattr(g, '_connection', None)
     if connection == None:
-        # connection = g._connection = sqlite3.connect(PATH)
-    # connection.row_factory = sqlite3.Row
+        connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
     return connection
 
 def execute_sql(sql, values=(), commit=False, single=False):
@@ -25,7 +25,7 @@ def execute_sql(sql, values=(), commit=False, single=False):
 
 @app.teardown_appcontext
 def close_connection(exception):
-    connection = getattr(g,'_connection', None)
+    connection = getattr(g, '_connection', None)
     if connection is not None:
         connection.close()
 
@@ -37,7 +37,5 @@ def jobs():
 
 @app.route('/job/<job_id>')
 def job(job_id):
-    job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?',
-    [job_id], single=True)
+    job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?', [job_id], single=True)
     return render_template('job.html', job=job)
-
