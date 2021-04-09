29c1e487b4aa1cdf2ff614cf485be0f50c644f3c
echan1912 <echan1912@gmail.com>
2021-04-09 16:01:11 +0800

    change

--- jobs/app.py
+++ jobs/app.py
@@ -14,11 +14,12 @@ def open_connection():
 
 def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
-    cursor = connection.execute_sql( sql, values)
+    cursor = connection.execute_sql(sql, values)
     if commit is True:
         results = connection.commit()
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
+        
         cursor.close()
         return results
 
