d02307e7ad493ff59e75e0a54d55ac7a35ff538d
echan1912 <echan1912@gmail.com>
2021-04-09 11:48:43 +0800

    checking route

--- jobs/app.py
+++ jobs/app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask import render_template
+from functools import wraps # decorators
 
 #create instance of flask class for our web app. pass name variable to flask constructor
 app = Flask(__name__)
 
+@app.route('/')
+@app.route('/jobs')
 def jobs():
     # returns call to render_template(), parameter index.html
     return render_template('index.html')
+    
